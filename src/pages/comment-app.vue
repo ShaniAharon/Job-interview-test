85f0b42c1bae9e425f1d339648c0108a772b7799
shforeal <rivena14798@gmail.com>
2022-05-06 22:41:49 +0300

    clean: removed logs

--- src/pages/comment-app.vue
+++ src/pages/comment-app.vue
@@ -23,72 +23,27 @@
       }
     },
     async created() {
-      // this.comments = //itemService.getInitialItemTree().children
-      // this.comments = await itemService.query()
       this.loadComments()
     },
     methods: {
       addComment({key, text}) {
-        console.log('here')
-        console.log(key, text)
         itemService.addComment(key, text)
       },
       async loadComments() {
-        console.log('loading')
         this.comments = await itemService.query()
       },
       async addToRoot() {
         const savedItem = await itemService.addCommentToRoot(this.commentText)
         this.loadComments()
-        console.log('saved', savedItem)
+
         this.commentText = ''
       },
     },
     computed: {
       commentsToShow() {
-        console.log('running')
         return this.comments
       },
     },
     unmounted() {},
   }
-
-  function getItemHtml(item) {
-    return `<li>
-                <label>
-                    ${item.key}
-                    <span>subCount: ${item.children.length}</span>
-                    <input type="checkbox" data-key="${item.key}" ${
-      item.picked ? 'checked' : ''
-    } onchange="app.updateSelection(this)" />
-                    <input type="text" data-key="${item.key}" ${
-      item.picked ? 'disabled' : ''
-    } onchange="app.updateInput(this)" />
-                </label>
-                ${`<ul>
-                    ${item.children.map(getItemHtml).join('')}
-                </ul>`}
-            </li>`
-  }
-
-  function renderItemsTree() {
-    var strHTML = ''
-    function accumulateItemsHTML(item) {
-      strHTML += getItemHtml(item)
-    }
-
-    itemService.traverseItems(accumulateItemsHTML)
-    const el = document.querySelector('.checkbox-container ul')
-    el.innerHTML = strHTML
-    // el.innerHTML = itemHTMLMap[Object.keys(itemHTMLMap)[0]]
-
-    function setIndeterminateItems(item) {
-      if (item.picked === undefined) {
-        el.querySelector(`input[data-key=${item.key}]`).indeterminate = true
-      }
-    }
-    itemService.traverseItems(setIndeterminateItems)
-  }
 </script>
-
-<style></style>
